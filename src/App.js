5af1537d80c1bd72af4e9c6414be78b3bbece0ab
Josh Bello <Jb230913@live.com>
2022-04-18 21:51:11 +0100

    install react-bootstrap and added logic for capture upload img and display in div

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import React,{useState} from 'react';
 function App() {
+  const [toptext,setTopText] = useState('');
+  const [bottomtext,setBottomText] = useState('');
+  const [img,setimg] = useState();
+
+
+  function handleImgUpload(e){
+    console.log(e.target.files[0]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  <>
+  <Container>
+  <Row>
+  <Col md={4}>
+      <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Default file input example</Form.Label>
+          <Form.Control type="file" accept="image/*" onChange={handleImgUpload} />
+        </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+  </Col>
+    <Col md={8}>
+    
+    
+    
+    </Col>
+    
+  </Row>
+
+</Container>
+  </>
   );
 }
 
