b49f7e5ff1a75d6056b0d2c67010c83ec4f7e616
Josh Bello <Jb230913@live.com>
2022-04-21 20:43:33 +0100

    remvoed unneeded code and html element from page and additional input field for desired filename for meme

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import html2canvas from "html2canvas";
 import downloadjs from 'downloadjs';
 import React,{useCallback, useRef, useState} from 'react';
+import { Alert } from 'bootstrap';
 function App() {
   const [toptext,setTopText] = useState('');
   const [bottomtext,setBottomText] = useState('');
-  const [img,setimg] = useState();
+  const [fileNametext,setfileNametext] = useState('');
   const mainimg  = useRef('');
   const toptextInput  = useRef('');
   const bottomtextInput  = useRef('');
+  const fileNametextInput  = useRef('');
 
 
   function handleImgUpload(e){
@@ -35,15 +37,28 @@ function App() {
   }
 
   const handleCapture = useCallback(async() =>{
-    const canvas = await html2canvas(document.getElementById('MEME'));
-    const dataURL = canvas.toDataURL('image/png');
-    downloadjs(dataURL,'download.png','image/png')
+
+    if (fileNametextInput.current.value === '') {
+      alert('this field can not be empty');
+    } else {
+      setfileNametext(fileNametextInput.current.value);
+      console.log(fileNametext);
+      const canvas = await html2canvas(document.getElementById('MEME'));
+      const dataURL = canvas.toDataURL('image/png');
+      downloadjs(dataURL,`${fileNametext}.png`,'image/png')
+    }
+
   },[]);
   
   return (
   
   <>
   <Container>
+  <Row>
+  <Col md={12} className="mb-5 mt-5 text-center">
+  <h1>Meme Generator</h1>
+  </Col>
+  </Row>
   <Row>
   <Col md={4}>
       <Form>
@@ -54,16 +69,18 @@ function App() {
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control  type="text"ref={bottomtextInput} placeholder="Enter Top Line Text here" onChange={handleBottomText}/>
+            <Form.Control  type="text"ref={bottomtextInput} placeholder="Enter Bottom Line Text here" onChange={handleBottomText}/>
           </Form.Group>
           <Form.Group controlId="formFile" className="mb-3">
-          <Form.Label>Default file input example</Form.Label>
+          <Form.Label>Please Select a Image file</Form.Label>
           <Form.Control type="file" accept="image/*" onChange={handleImgUpload} />
         </Form.Group>
-          {/* <Button variant="primary" type="submit" onClick={handleCapture}>
-            Submit
-          </Button> */}
-          <a href="#" onClick={handleCapture}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+           
+           <Form.Control type="text" ref={fileNametextInput} placeholder="Enter Desired Filename for Your Meme"/>
+         </Form.Group>
+
+         <a className='mt-10 btn btn-primary' onClick={handleCapture}>
             Capture
           </a>
         </Form>
